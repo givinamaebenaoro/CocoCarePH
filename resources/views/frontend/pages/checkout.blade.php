ea694ed19af9d9fbb9b181dc27cfaa812d45ed63
RosePami <101541747+RosePami@users.noreply.github.com>
2024-04-29 16:07:46 +0800

    Update checkout.blade.php

--- resources/views/frontend/pages/checkout.blade.php
+++ resources/views/frontend/pages/checkout.blade.php
@@ -477,28 +477,6 @@
     </section>
     <!-- End Shop Services -->
 
-    <!-- Start Shop Newsletter  -->
-    <section class="shop-newsletter section">
-        <div class="container">
-            <div class="inner-top">
-                <div class="row">
-                    <div class="col-lg-8 offset-lg-2 col-12">
-                        <!-- Start Newsletter Inner -->
-                        <div class="inner">
-                            <h4>Newsletter</h4>
-                            <p> Subscribe to our newsletter and get <span>10%</span> off your first purchase</p>
-                            <form action="mail/mail.php" method="get" target="_blank" class="newsletter-inner">
-                                <input name="EMAIL" placeholder="Your email address" required="" type="email">
-                                <button class="btn">Subscribe</button>
-                            </form>
-                        </div>
-                        <!-- End Newsletter Inner -->
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
-    <!-- End Shop Newsletter -->
 @endsection
 @push('styles')
 	<style>
@@ -591,4 +569,3 @@
 </script>
 
 @endpush
-
