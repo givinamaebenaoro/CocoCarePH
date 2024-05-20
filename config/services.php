a24feb9e5e42fa4ffb5ca2c2e0e60bdac3ff0b36
givinamaebenaoro <101541188+givinamaebenaoro@users.noreply.github.com>
2024-05-20 11:53:50 +0800

    Update services.php

--- config/services.php
+++ config/services.php
@@ -40,7 +40,7 @@ return [
         "auth_uri": "https://accounts.google.com/o/oauth2/auth",
         "token_uri": "https://oauth2.googleapis.com/token",
         "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-        "client_secret": "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"
+        "client_secret": "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",
         'redirect' => 'https://cococareph.store/login/google/callback',
      ],
      'facebook' => [
