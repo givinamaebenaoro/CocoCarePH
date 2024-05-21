b705c8fdc8f1e076776c50d6e9183b4bc19d9c75
Edmhund Fhaye G. Mendoza <101541269+FhayeMendoza@users.noreply.github.com>
2024-05-21 13:49:50 +0800

    Update create.blade.php

--- resources/views/backend/post/create.blade.php
+++ resources/views/backend/post/create.blade.php
@@ -9,78 +9,96 @@
         {{csrf_field()}}
         <div class="form-group">
           <label for="inputTitle" class="col-form-label">Title <span class="text-danger">*</span></label>
-          <input id="inputTitle" type="text" name="title" placeholder="Enter title"  value="{{old('title')}}" class="form-control">
+          <input id="inputTitle" type="text" name="title" placeholder="Enter title" value="{{old('title')}}" class="form-control">
           @error('title')
           <span class="text-danger">{{$message}}</span>
           @enderror
         </div>
 
-        <div class="form-group">
-          <label for="quote" class="col-form-label">Quote</label>
-          <textarea class="form-control" id="quote" name="quote">{{old('quote')}}</textarea>
-          @error('quote')
-          <span class="text-danger">{{$message}}</span>
-          @enderror
+        <div class="row">
+          <div class="col-md-4">
+            <div class="form-group">
+              <label for="quote" class="col-form-label">Quote</label>
+              <textarea class="form-control" id="quote" name="quote">{{old('quote')}}</textarea>
+              @error('quote')
+              <span class="text-danger">{{$message}}</span>
+              @enderror
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="form-group">
+              <label for="summary" class="col-form-label">Summary <span class="text-danger">*</span></label>
+              <textarea class="form-control" id="summary" name="summary">{{old('summary')}}</textarea>
+              @error('summary')
+              <span class="text-danger">{{$message}}</span>
+              @enderror
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="form-group">
+              <label for="description" class="col-form-label">Description</label>
+              <textarea class="form-control" id="description" name="description">{{old('description')}}</textarea>
+              @error('description')
+              <span class="text-danger">{{$message}}</span>
+              @enderror
+            </div>
+          </div>
         </div>
 
-        <div class="form-group">
-          <label for="summary" class="col-form-label">Summary <span class="text-danger">*</span></label>
-          <textarea class="form-control" id="summary" name="summary">{{old('summary')}}</textarea>
-          @error('summary')
-          <span class="text-danger">{{$message}}</span>
-          @enderror
+        <div class="row">
+          <div class="col-md-6">
+            <div class="form-group">
+              <label for="post_cat_id">Category <span class="text-danger">*</span></label>
+              <select name="post_cat_id" class="form-control">
+                  <option value="">--Select any category--</option>
+                  @foreach($categories as $key=>$data)
+                      <option value='{{$data->id}}'>{{$data->title}}</option>
+                  @endforeach
+              </select>
+            </div>
+          </div>
+          <div class="col-md-6">
+            <div class="form-group">
+              <label for="tags">Tag</label>
+              <select name="tags[]" multiple data-live-search="true" class="form-control selectpicker">
+                  <option value="">--Select any tag--</option>
+                  @foreach($tags as $key=>$data)
+                      <option value='{{$data->title}}'>{{$data->title}}</option>
+                  @endforeach
+              </select>
+            </div>
+          </div>
         </div>
 
-        <div class="form-group">
-          <label for="description" class="col-form-label">Description</label>
-          <textarea class="form-control" id="description" name="description">{{old('description')}}</textarea>
-          @error('description')
-          <span class="text-danger">{{$message}}</span>
-          @enderror
-        </div>
-
-        <div class="form-group">
-          <label for="post_cat_id">Category <span class="text-danger">*</span></label>
-          <select name="post_cat_id" class="form-control">
-              <option value="">--Select any category--</option>
-              @foreach($categories as $key=>$data)
-                  <option value='{{$data->id}}'>{{$data->title}}</option>
-              @endforeach
-          </select>
-        </div>
-
-        <div class="form-group">
-          <label for="tags">Tag</label>
-          <select name="tags[]" multiple  data-live-search="true" class="form-control selectpicker">
-              <option value="">--Select any tag--</option>
-              @foreach($tags as $key=>$data)
-                  <option value='{{$data->title}}'>{{$data->title}}</option>
-              @endforeach
-          </select>
-        </div>
-        <div class="form-group">
-          <label for="added_by">Author</label>
-          <select name="added_by" class="form-control">
-              <option value="">--Select any one--</option>
-              @foreach($users as $key=>$data)
-                <option value='{{$data->id}}' {{($key==0) ? 'selected' : ''}}>{{$data->name}}</option>
-            @endforeach
-          </select>
-        </div>
-        <div class="form-group">
-          <label for="inputPhoto" class="col-form-label">Photo <span class="text-danger">*</span></label>
-          <div class="input-group">
-              <span class="input-group-btn">
-                  <a id="lfm" data-input="thumbnail" data-preview="holder" class="btn btn-primary">
-                  <i class="fa fa-picture-o"></i> Choose
-                  </a>
-              </span>
-          <input id="thumbnail" class="form-control" type="text" name="photo" value="{{old('photo')}}">
-        </div>
-        <div id="holder" style="margin-top:15px;max-height:100px;"></div>
-          @error('photo')
-          <span class="text-danger">{{$message}}</span>
-          @enderror
+        <div class="row">
+          <div class="col-md-6">
+            <div class="form-group">
+              <label for="added_by">Author</label>
+              <select name="added_by" class="form-control">
+                  <option value="">--Select any one--</option>
+                  @foreach($users as $key=>$data)
+                    <option value='{{$data->id}}' {{($key==0) ? 'selected' : ''}}>{{$data->name}}</option>
+                @endforeach
+              </select>
+            </div>
+          </div>
+          <div class="col-md-6">
+            <div class="form-group">
+              <label for="inputPhoto" class="col-form-label">Photo <span class="text-danger">*</span></label>
+              <div class="input-group">
+                  <span class="input-group-btn">
+                      <a id="lfm" data-input="thumbnail" data-preview="holder" class="btn btn-primary">
+                      <i class="fa fa-picture-o"></i> Choose
+                      </a>
+                  </span>
+                  <input id="thumbnail" class="form-control" type="text" name="photo" value="{{old('photo')}}">
+              </div>
+              <div id="holder" style="margin-top:15px;max-height:100px;"></div>
+              @error('photo')
+              <span class="text-danger">{{$message}}</span>
+              @enderror
+            </div>
+          </div>
         </div>
 
         <div class="form-group">
@@ -93,13 +111,15 @@
           <span class="text-danger">{{$message}}</span>
           @enderror
         </div>
+
         <div class="form-group mb-3">
           <button type="reset" class="btn btn-warning">Reset</button>
-           <button class="btn btn-success" type="submit">Submit</button>
+          <button class="btn btn-success" type="submit">Submit</button>
         </div>
       </form>
     </div>
 </div>
+
 @endsection
 
 @push('styles')
